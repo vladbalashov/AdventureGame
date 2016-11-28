ddd2e823aea1ff45fbfe309fb293e57167c31314
vladbalashov <vladbalashov@mbpvladbalashov>
2016-11-28 12:44:21 +0300

    team assignment

--- src/byui/cit260/adventureGame/control/EncounterControl.java
+++ src/byui/cit260/adventureGame/control/EncounterControl.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.adventureGame.control;
 
+import exceptions.EncounterControlException;
+
 /**
  *
  * @author Jack
@@ -26,14 +28,16 @@ public String addToCharacter(String item, int amount) {
 }
 public static double calcAbilityToWin(double attackPhysical1, double attackMagical1,
         double defensePhysical1, double defenseMagical1, double attackPhysical2, double attackMagical2,
-        double defensePhysical2, double defenseMagical2, double health1, double health2 ) {
+        double defensePhysical2, double defenseMagical2, double health1, double health2 ) throws 
+                                                            EncounterControlException{
     
     if (attackPhysical1<0 || attackMagical1<0 || defensePhysical1<0 || defenseMagical1<0
     || attackPhysical2<0 || attackMagical2<0 || defensePhysical2<0 || defenseMagical2<0) {
-        return -1000000;
+        throw new EncounterControlException("None of these parameters can be negative or equal to zero.");
     }
+    
     if (health1<=0 || health1>10000 || health2<=0 || health2>10000) {
-        return -1000000;
+        throw new EncounterControlException("Health should be more than zero and less that 10,000");
     }
     
     double attack1 = attackPhysical1 + attackMagical1;
@@ -44,32 +48,47 @@ public static double calcAbilityToWin(double attackPhysical1, double attackMagic
     double check2 = attack2 - defense1;
     
     if (check1 == 0 && check2 == 0) {
-        double level = 0;
-        return level;
+        throw new EncounterControlException("You are equal to your enemy. Please use potion to win ");
     }
 
     if (check1==0 && check2>0){
         double level = 0 - health1/check2;
-        return level;
+        if (level <= 0){
+            throw new EncounterControlException("Your ability to win is "+level+ " Unfortunately it is not enough to win."
+                    + " Please use potion to win");
+        }
+        else {
+            throw new EncounterControlException("Your ability to win is "+level+" We wish you luck in your fight");
+        }
     }
 
     if (check1==0 && check2<0){
-        double level = 0;
-        return level;
+        throw new EncounterControlException("You are equal to your enemy. Please use potion to win ");
     }
 
     if (check2==0 && check1>0) {
         double level = health2/check1;
-        return level;
+        if (level <= 0){
+            throw new EncounterControlException("Your ability to win is "+level+ " Unfortunately it is not enough to win."
+                    + " Please use potion to win");
+        }
+        else {
+            throw new EncounterControlException("Your ability to win is "+level+" We wish you luck in your fight");
+        }
     }
 
     if (check2==0 && check1<0) {
-        double level = 0;
-        return level;
+        throw new EncounterControlException("You are equal to your enemy. Please use potion to win ");
     }
 
     double level = health2/check1 - health1/check2;
-    return level;
+    if (level <= 0){
+            throw new EncounterControlException("Your ability to win is "+level+ " Unfortunately it is not enough to win."
+                    + " Please use potion to win");
+        }
+        else {
+            throw new EncounterControlException("Your ability to win is "+level+" We wish you luck in your fight");
+        }
    
 }
 public double calcPotionNeeded(double defense1, double defense2, double attack1, double attack2) {
